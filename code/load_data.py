beef0c639a1c059720ac3c1aeed1356a88adc9ae
HugoDelhaye <hugo.delhaye@umontreal.ca>
2025-04-09 11:37:05 -0400

    update structure file and requirement.txt

---
+++ code/load_data.py
@@ -0,0 +1,128 @@
+import os
+from pathlib import Path
+import pandas as pd
+import re
+import torch
+
+from src.models import PPO
+
+def get_models(models_path, device='cpu'):
+    """
+    Charge les modèles PPO depuis un répertoire donné.
+    
+    Args:
+        models_path (str): Chemin vers le dossier contenant les modèles.
+        device (str): 'cpu' ou 'cuda' pour exécuter sur GPU si disponible.
+    
+    Returns:
+        list: Une liste des modèles chargés.
+    """
+
+    models = []
+    models_names = []
+    path_models = []
+    for model_name in os.listdir(models_path):
+        model_file = os.path.join(models_path, model_name)
+        if model_file.endswith('.pt'):
+            model = PPO(n_in=4, n_actions=12)
+            model.load_state_dict(torch.load(model_file, map_location=device))
+            torch.set_grad_enabled(False)
+            model.to(device)
+            model.eval()
+            models_names.append(model_name)
+            models.append(model)
+            path_models.append(model_file)
+    
+    return pd.DataFrame({'name_models': models_names,
+                        'loaded_models': models,
+                        'path_models' : path_models
+                            })
+
+def parse_state_files(states_path):
+
+    """Load the states from the derivatives folder with additional info (sub, ses)."""
+
+    base_folder = Path(states_path)
+
+    if not base_folder.exists():
+        print(f"Target folder does not exist: {base_folder}")
+        return pd.DataFrame(columns=['state_path', 'sub', 'ses', 'level', 'scene'])
+    
+    #base_folder = Path(os.path.join(path_stats, "source_data", states))
+
+    else :
+        print(f"Loading states from {base_folder}")
+        state_files = list(base_folder.glob("sub*/ses*/beh/savestates/*.state"))
+
+        data = []
+        for file in state_files:
+        
+            match = re.search(r"(sub-\d{2})_(ses-\d{3})_run-\d{2}_level-(\w{4})_(scene-\d{1,2})_clip-(\d+)_beh\.state", str(file))
+
+            if match:
+                sub, ses, level, scene, num_clip = match.groups()
+            else:
+                print(f"[WARNING] Pas de match pour le fichier : {file}")
+            
+            data.append({'state_path': str(file), 'sub': sub, 'ses': ses, 'level': level, 'scene': scene, 'num_clip': num_clip})
+
+        return pd.DataFrame(data) # or return la liste state_files
+    
+def get_mastersheet(filepath):
+    """
+    Charge et retourne un mastersheet sous forme de DataFrame.
+    
+    Args:
+        filepath (str): Chemin du fichier CSV ou Excel.
+    
+    Returns:
+        pd.DataFrame: Le mastersheet chargé.
+    """
+
+    if filepath.endswith('.csv'):
+        return pd.read_csv(filepath)
+    elif filepath.endswith(('.xls', '.xlsx')):
+        return pd.read_excel(filepath)
+    else:
+        raise ValueError("Format de fichier mastershhet non supporté. Utilisez CSV ou Excel.")
+    
+def get_scene(state):
+    """
+    Extrait le monde, le niveau et la scène depuis l'identifiant de l'état.
+    
+    Args:
+        state (str): Identifiant de l'état du jeu (ex: "sub-01_ses-001_run-01_level-w1l1_scene-0_clip-00101000000122_beh").
+    
+    Returns:
+        str: Chaîne formatée (ex: "w1l1s0").
+    """
+    try:
+        level_part = [s for s in state.split('_') if s.startswith('level-')][0]
+        scene_part = [s for s in state.split('_') if s.startswith('scene-')][0]
+        world = level_part[7]  # 'w1l1' -> '1'
+        level = level_part[9]  # 'w1l1' -> '1'
+        scene = scene_part.split('-')[1]  # 'scene-0' -> '0'
+        return f"w{world}l{level}s{scene}"
+    
+    except (IndexError, ValueError):
+        return "Unknown Scene from the .state file"
+
+def get_xpos_max (ms, scene):
+    """
+    Extrait la valeur maximale de Xscroll pour une scène donnée depuis le mastersheet.
+
+    Args:
+        mastersheet (pd.DataFrame): DataFrame contenant les informations de la scène.
+        scene (str): Identifiant de la scène (ex: "w1l1s0").
+
+    Returns:
+        int : Valeur maximale de player_x_pos pour la scène donnée.
+    """
+
+    xpos_max = ms[(ms['World'] == int(scene[1])) & (ms['Level'] == int(scene[3])) & (ms['Scene'] == int(scene[5]))]['Exit point'].values
+
+    if xpos_max.size == 0:
+        print(f"Scene {scene} not found in the mastersheet.")
+        return None
+
+    return  int(xpos_max)
--- code/utils.py
+++ code/utils.py
@@ -1,80 +1,22 @@
 import os
-import sys
 import torch
 import pandas as pd
 from pathlib import Path
 import json
 import numpy as np
-from tqdm import tqdm
-import re
 import imageio
-sys.path.append(os.path.join(os.getcwd()))
+from scipy.special import softmax
+import argparse
+import retro
 
-from src.models import PPO
+from src.ppo.env import preprocess_frames, complex_movement_to_button_presses
+from src.ppo.emulation import add_unused_buttons
+from src.ppo import PPO
 
-
-def get_models(models_path, device='cpu'):
-    """
-    Charge les modèles PPO depuis un répertoire donné.
-    
-    Args:
-        models_path (str): Chemin vers le dossier contenant les modèles.
-        device (str): 'cpu' ou 'cuda' pour exécuter sur GPU si disponible.
-    
-    Returns:
-        list: Une liste des modèles chargés.
-    """
-
-    models = []
-    models_names = []
-    path_models = []
-    for model_name in os.listdir(models_path):
-        model_file = os.path.join(models_path, model_name)
-        if model_file.endswith('.pt'):
-            model = PPO(n_in=4, n_actions=12)
-            model.load_state_dict(torch.load(model_file, map_location=device))
-            torch.set_grad_enabled(False)
-            model.to(device)
-            model.eval()
-            models_names.append(model_name)
-            models.append(model)
-            path_models.append(model_file)
-    
-    return pd.DataFrame({'name_models': models_names,
-                        'loaded_models': models,
-                        'path_models' : path_models
-                            })
+from load_data import get_mastersheet, get_models, parse_state_files, get_scene, get_xpos_max
 
 
-def parse_state_files(states_path):
 
-    """Load the states from the derivatives folder with additional info (sub, ses)."""
-
-    base_folder = Path(states_path)
-
-    if not base_folder.exists():
-        print(f"Target folder does not exist: {base_folder}")
-        return pd.DataFrame(columns=['state_path', 'sub', 'ses', 'level', 'scene'])
-    
-    #base_folder = Path(os.path.join(path_stats, "source_data", states))
-
-    else :
-        print(f"Loading states from {base_folder}")
-        state_files = list(base_folder.glob("sub*/ses*/beh/savestates/*.state"))
-
-        data = []
-        for file in state_files:
-        
-            match = re.search(r"(sub-\d{2})_(ses-\d{3})_run-\d{2}_level-(\w{4})_(scene-\d{1,2})_clip-(\d+)_beh\.state", str(file))
-
-            if match:
-                sub, ses, level, scene, num_clip = match.groups()
-            else:
-                print(f"[WARNING] Pas de match pour le fichier : {file}")
-            
-            data.append({'state_path': str(file), 'sub': sub, 'ses': ses, 'level': level, 'scene': scene, 'num_clip': num_clip})
-
-        return pd.DataFrame(data) # or return la liste state_files
     
 def filter_states(states_df, filters):
     """
@@ -95,82 +37,6 @@ def filter_states(states_df, filters):
             states_df = states_df[states_df[key].isin(values)]
 
     return states_df
-
-def create_bk2_folders (models, subjects, sessions, base_path):
-
-    """
-    Crée les dossiers nécessaires pour stocker les fichiers BK2.
-    
-    Args:
-        base_path (str): Chemin de base où créer les dossiers.
-        models (list): Liste des noms de modèles PPO.
-        subjects (list): Liste des sujets.
-        sessions (list): Liste des sessions.
-    """
-    for model in models:
-        for sub in subjects:
-            for ses in sessions:
-                path = os.path.join(base_path, 'artificial_agents', model, sub, ses, 'beh', 'bk2')
-                os.makedirs(path, exist_ok=True)
-
-def get_mastersheet(filepath):
-    """
-    Charge et retourne un mastersheet sous forme de DataFrame.
-    
-    Args:
-        filepath (str): Chemin du fichier CSV ou Excel.
-    
-    Returns:
-        pd.DataFrame: Le mastersheet chargé.
-    """
-
-    if filepath.endswith('.csv'):
-        return pd.read_csv(filepath)
-    elif filepath.endswith(('.xls', '.xlsx')):
-        return pd.read_excel(filepath)
-    else:
-        raise ValueError("Format de fichier mastershhet non supporté. Utilisez CSV ou Excel.")
-
-def get_scene(state):
-    """
-    Extrait le monde, le niveau et la scène depuis l'identifiant de l'état.
-    
-    Args:
-        state (str): Identifiant de l'état du jeu (ex: "sub-01_ses-001_run-01_level-w1l1_scene-0_clip-00101000000122_beh").
-    
-    Returns:
-        str: Chaîne formatée (ex: "w1l1s0").
-    """
-    try:
-        level_part = [s for s in state.split('_') if s.startswith('level-')][0]
-        scene_part = [s for s in state.split('_') if s.startswith('scene-')][0]
-        world = level_part[7]  # 'w1l1' -> '1'
-        level = level_part[9]  # 'w1l1' -> '1'
-        scene = scene_part.split('-')[1]  # 'scene-0' -> '0'
-        return f"w{world}l{level}s{scene}"
-    
-    except (IndexError, ValueError):
-        return "Unknown Scene from the .state file"
-
-def get_xpos_max (ms, scene):
-    """
-    Extrait la valeur maximale de Xscroll pour une scène donnée depuis le mastersheet.
-
-    Args:
-        mastersheet (pd.DataFrame): DataFrame contenant les informations de la scène.
-        scene (str): Identifiant de la scène (ex: "w1l1s0").
-
-    Returns:
-        int : Valeur maximale de player_x_pos pour la scène donnée.
-    """
-
-    xpos_max = ms[(ms['World'] == int(scene[1])) & (ms['Level'] == int(scene[3])) & (ms['Scene'] == int(scene[5]))]['Exit point'].values
-
-    if xpos_max.size == 0:
-        print(f"Scene {scene} not found in the mastersheet.")
-        return None
-
-    return  int(xpos_max)
 
 
 def get_previous_frames (state, prev_frames=-16):
@@ -271,23 +137,6 @@ def process_state(row_state, ppo_row, info_scene):
     emul.reset()
 
     context_frames = get_previous_frames(state)
-
-
-    fig, axes = plt.subplots(4, 4, figsize=(10, 10))
-        
-    ''' # Remplir la grille
-    for i in range(4):
-        for j in range(4):
-            idx = i * 4 + j
-            print(idx)
-            ax = axes[i, j]
-            ax.imshow(context_frames[idx, :, : ,:])
-            ax.axis("off")  # Toujours cacher les axes, même s’il n’y a pas d’image
-
-    plt.tight_layout()
-    plt.show()
-    plt.close()
-    '''
 
     rng = np.random.default_rng(seed=1)
     pred_rate = 4
