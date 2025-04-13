a49f52f1f373f4d446b24fed420de09dde29d3e4
Yann Harel <yharel109@gmail.com>
2025-04-12 22:35:34 -0400

    creates companion files

--- code/utils.py
+++ code/utils.py
@@ -8,9 +8,12 @@ import imageio
 from scipy.special import softmax
 import argparse
 import retro
-
+import os.path as op
+import pickle
 from src.ppo.env import preprocess_frames, complex_movement_to_button_presses
 from src.ppo.emulation import add_unused_buttons
+
+from mario_replays.utils import reformat_info, make_mp4, make_gif, make_webp, create_sidecar_dict
 
     
 def filter_states(states_df, filters):
@@ -46,7 +49,7 @@ def get_previous_frames (state, args, prev_frames=-16):
     """
     # get the json correspondind to the state
     json_path = state.replace('.state', '.json')
-    json_path = json_path.replace('savestates', 'clips')
+    json_path = json_path.replace('savestates', 'infos')
 
     # open json file and extract ClipCode and bk2_filepath
     with open(json_path, 'r') as f:
@@ -104,9 +107,18 @@ def mp4_to_list(mp4_filepath, start_frame, num_frames):
 
 def process_state(state_path, args, ppo, x_max, path_output, stimuli, verbose=False):
 
-
-    os.makedirs(path_output, exist_ok=True)
-
+    entities = state_path.split('/')[-1].split('.')[0]
+    beh_folder = path_output
+    # Prepare output filenames
+    gif_fname       = op.join(beh_folder, 'videos', f"{entities}.gif")
+    mp4_fname       = op.join(beh_folder, 'videos', f"{entities}.mp4")
+    webp_fname      = op.join(beh_folder, 'videos', f"{entities}.webp")
+    savestate_fname = op.join(beh_folder, 'savestates', f"{entities}.state")
+    ramdump_fname   = op.join(beh_folder, 'ramdumps', f'{entities}.npz')
+    json_fname      = op.join(beh_folder, 'infos', f"{entities}.json")
+    variables_fname = op.join(beh_folder, 'variables', f"{entities}.pkl")
+    bk2_fname       = op.join(beh_folder, 'bk2', f"{entities}.bk2")
+    os.makedirs(os.path.dirname(bk2_fname), exist_ok=True)
 
     # Setup the environments with Mario
     resolved_path = Path(stimuli).resolve()
@@ -114,17 +126,19 @@ def process_state(state_path, args, ppo, x_max, path_output, stimuli, verbose=Fa
     
     emul = retro.make(game='SuperMarioBros-Nes', 
                       inttype=retro.data.Integrations.CUSTOM_ONLY, 
-                      record=path_output,
+                      record=op.join(beh_folder, 'bk2'),
                       render_mode=False)
     emul.load_state(state_path)
     emul.reset()
-
     context_frames = get_previous_frames(state_path, args)
 
     rng = np.random.default_rng(seed=1)
     pred_rate = 4
     n_frames = 0
     done = False
+    frames_list = []
+    info_list = []
+    keys_list = []
 
     while not done:
         # Predict new actions
@@ -146,8 +160,13 @@ def process_state(state_path, args, ppo, x_max, path_output, stimuli, verbose=Fa
                 print("Actions : ", actions)
             act = actions[0].tolist()
 
+        keys = add_unused_buttons(act)
+        obs, _rew, _term, _trunc, info = emul.step(keys)
+        frames_list.append(obs)
+        info_list.append(info)
+        keys_list.append(keys)
+        buttons = emul.buttons
 
-        obs, _rew, _term, _trunc, info = emul.step(add_unused_buttons(act))
 
         if n_frames == 0:
             lives = info['lives']
@@ -167,3 +186,42 @@ def process_state(state_path, args, ppo, x_max, path_output, stimuli, verbose=Fa
                 done = True
                 
         n_frames += 1
+    
+    scene_variables = reformat_info(info_list, keys_list, bk2_fname, buttons)
+
+    # Generate json
+    metadata = {
+                    'Model': path_output.split('/')[-4],
+                    'StateFileName': state_path,
+                    'Level': state_path.split('/')[-1].split('_')[-3].split('-')[1],
+                    'Scene': state_path.split('/')[-1].split('_')[-2].split('-')[1],
+                    'StateClipCode': state_path.split('/')[-1].split('_')[-1].split('-')[1],
+                    'TotalFrames': n_frames,
+                    'Bk2Filepath': bk2_fname,
+                    'GameName': emul.gamename,
+                }
+    scene_sidecar = create_sidecar_dict(scene_variables)
+    enriched_metadata = metadata.copy()
+    enriched_metadata.update(scene_sidecar)
+    os.makedirs(os.path.dirname(json_fname), exist_ok=True)
+
+    with open(json_fname, 'w') as json_file:    
+        json.dump(enriched_metadata, json_file)
+
+    # Generate files
+    if args.save_videos:
+        os.makedirs(os.path.dirname(gif_fname), exist_ok=True)
+        if args.video_format == 'gif':
+            make_gif(frames_list, gif_fname)
+        elif args.video_format == 'mp4':
+            make_mp4(frames_list, mp4_fname)
+        elif args.video_format == 'webp':
+            make_webp(frames_list, webp_fname)
+    if args.save_ramdumps:
+        os.makedirs(os.path.dirname(ramdump_fname), exist_ok=True)
+        np.savez_compressed(ramdump_fname, replay_states[start_idx:end_idx])
+    if args.save_variables:
+        os.makedirs(os.path.dirname(variables_fname), exist_ok=True)
+        with open(variables_fname, 'wb') as f:
+            pickle.dump(scene_variables, f)
+
